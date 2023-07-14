bfe58159803fa6d604707938a7900d9525971228
ArsenFabulous <tolstykhsteam@gmail.com>
2023-07-14 14:55:06 +0300

    Minor changes of Hint.tsx

--- src/components/Hint.tsx
+++ src/components/Hint.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 const Hint: React.FC = () => {
-    const [switcher, setSwitcher] = useState<boolean>(false)
+    const [switcher, setSwitcher] = useState<boolean>(true)
     return (<>
         <button onClick={() => setSwitcher(!switcher)}>{switcher ? 'Убрать подсказку' : 'Показать подсказку'}</button>
         {switcher
@@ -10,9 +10,9 @@ const Hint: React.FC = () => {
                 <li>
                     <ul> 1 ЛР
                         <li>Для выполнения первого этапа ввести:
-                            {/* <img src=''> */}
-                            </li>                        
-                        <li>В таблице выбрать насос X45/54</li>                        
+                            <img className='hint__list__img' src='https://github.com/ArsenFabulous/HeatPowerEng.Lab.Works/blob/main/public/LW1/Complete.jpg?raw=true' />
+                        </li>
+                        <li>В таблице выбрать насос X45/54</li>
                     </ul>
                     <ul>    2 ЛР
                         <li>Для снятия показаний поочерёдно ввести 5 значений Подачи: 0.001, 0.002, 0.003, 0.004, 0.005. При этом показатели приборов будут меняться (на приборы нужно навестись мышкой).</li>
